097013b4cbf2364515580eae29609064ae24a83e
Morphisor <a.gallitri@gmail.com>
2019-04-14 00:26:45 +0200

    bug fix

--- PlexFileRenamer.Models/ApiModels/TheTvDb/Series/Links.cs
+++ PlexFileRenamer.Models/ApiModels/TheTvDb/Series/Links.cs
@@ -6,9 +6,9 @@ namespace PlexFileRenamer.Models.ApiModels.TheTvDb.Series
 {
     public class Links
     {
-        public int First { get; set; }
-        public int Last { get; set; }
-        public int Next { get; set; }
-        public int Previous { get; set; }
+        public int? First { get; set; }
+        public int? Last { get; set; }
+        public int? Next { get; set; }
+        public int? Previous { get; set; }
     }
 }

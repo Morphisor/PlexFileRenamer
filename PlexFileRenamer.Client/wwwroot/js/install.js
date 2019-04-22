b461fa44a9a84ff73b3e0ad405158a48787b5637
Morphisor <a.gallitri@gmail.com>
2019-04-22 20:26:30 +0200

    PWA work

---
+++ PlexFileRenamer.Client/wwwroot/js/install.js
@@ -0,0 +1,25 @@
+﻿function saveBeforeInstallPromptEvent(evt) {
+    deferredInstallPrompt = evt;
+}
+
+function installPWA(evt) {
+    deferredInstallPrompt.prompt();
+    evt.srcElement.setAttribute('hidden', true);
+    deferredInstallPrompt.userChoice
+        .then((choice) => {
+            if (choice.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt', choice);
+            } else {
+                choice.log('User dismisssed the A2HS prompt', choice);
+            }
+        });
+}
+
+function logAppInstalled(evt) {
+    console.log('App installed.', evt);
+}
+
+let deferredInstallPrompt = null;
+installButton.addEventListener('click', installPWA);
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+window.addEventListener('appinstalled', logAppInstalled);
